a74531f22ca1d93d7fff0ed11cbb31f559baeb8c
Md. Tusher Ahmad Bappy <106505665+tusherahmadbappy@users.noreply.github.com>
2022-06-05 10:35:51 +0600

    afterindexinsert of an array

---
+++ afterindexinsert.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int size,index,value,i,arr[20];
+    cout<<"Enter the size of array: ";
+    cin>>size;
+    //user input array element
+    for(i=0; i<size; i++){
+        cout << "Index of arr[" << i << "] is: ";
+        cin>>arr[i];
+    }
+
+    cout<<endl<<"Before insert elements: "<<endl;
+    for(i=0; i<size; i++){
+        cout << "Index of arr[" << i << "] element is: " << arr[i]<<endl;;
+    }
+
+    cout<<"Which after index insert element you want: ";
+    cin>>index;
+    cout<<endl<<"How value insert array you want: ";
+    cin>>value;
+
+    for(int i=size;i>index-1;i--){
+        arr[i+1]=arr[i];
+    }
+    arr[index+1]=value;
+
+    cout<<endl<<"After insert elements: "<<endl;
+    for(i=0; i<=size; i++){
+        cout<< "Index of arr[" << i << "] element is: "<<arr[i]<<endl;
+    }
+
+    return 0;
+}
